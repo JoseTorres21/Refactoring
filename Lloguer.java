99660d348ab057d9b8beaac1eddb9a0c1585a747
JoseTorres21 <jtorresodonez@gmail.com>
2019-05-20 19:32:22 +0200

    Exercici2

---
+++ Lloguer.java
@@ -0,0 +1,55 @@
+import java.util.Date;
+
+public class Lloguer {
+	private Date data;
+	private int dies;
+	private Vehicle vehicle;
+
+	public Lloguer(Date data, int dies, Vehicle vehicle) {
+		this.data = data;
+		this.dies = dies;
+        this.vehicle = vehicle;
+		setVehicle(null);
+	}
+	
+	public boolean tetLloguer() {
+		if (vehicle != null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+	
+	public Date getData() {
+		return data;
+	}
+
+	public void setDies(int dies) {
+		this.dies = dies;
+	}
+	
+	public int getDies() {
+		return dies;
+	}
+
+	public void setVehicle(Vehicle vehicle) {
+		this.vehicle = vehicle;
+	}
+	
+	public Vehicle getVehicle() {
+		return vehicle;
+	}
+
+
+	public void lloga(Vehicle vehicle) {
+		setVehicle(vehicle);
+	}
+
+	public void eliminaLloguer() {
+		setVehicle(null);
+	}
+}
