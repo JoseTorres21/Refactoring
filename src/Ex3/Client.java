c6339b704b1f44bb353afa5ed2657b6d71d8e519
JoseTorres21 <jtorresodonez@gmail.com>
2019-05-24 01:14:38 +0200

    Exercici 7: Quantitat per lloguer

--- src/Ex3/Client.java
+++ src/Ex3/Client.java
@@ -38,16 +38,73 @@ public class Client {
 		return lloguers.contains(lloguer);
 	}
 
-	public String informe() {
+	/*public String informe() {
 		// XXX: de moment buit
 		return null;
-	}
+	}*/
 
 	public int totalLloguer(Client c) {
 		return lloguers.size();
 	}
-  
+
 	public ArrayList<Lloguer> getLloguers() {
 		return lloguers;
+	}
+
+	public String informe() {
+		double total = 0;
+		int bonificacions = 0;
+		String resultat = "Informe de lloguers del client " +
+				getNom() +
+				" (" + getNif() + ")\n";
+		for (Lloguer lloguer: lloguers) {
+			double quantitat = quantitatPerLloguer(lloguer);
+
+			// afegeix lloguers freqüents
+			bonificacions ++;
+
+			// afegeix bonificació per dos dies de lloguer de Luxe
+			if (lloguer.getVehicle().getCategoria() == Vehicle.LUXE &&
+					lloguer.getDies()>1 ) {
+				bonificacions ++;
+			}
+
+			// composa els resultats d'aquest lloguer
+			resultat += "\t" +
+					lloguer.getVehicle().getMarca() +
+					" " +
+					lloguer.getVehicle().getModel() + ": " +
+					(quantitat * 30) + "€" + "\n";
+			total += quantitat * 30;
+		}
+
+		// afegeix informació final
+		resultat += "Import a pagar: " + total + "€\n" +
+				"Punts guanyats: " + bonificacions + "\n";
+		return resultat;
+	}
+
+	private double quantitatPerLloguer (Lloguer lloguer) {
+		double quantitat = 0;
+		switch(lloguer.getVehicle().getCategoria()) {
+		case Vehicle.BASIC:
+			quantitat = quantitat + 3;
+			if(lloguer.getDies() > 3) {
+				quantitat = quantitat + (lloguer.getDies() -3) * 1.5;
+			}
+			break;
+
+		case Vehicle.GENERAL:
+			quantitat = quantitat + 4;
+			if(lloguer.getDies() > 2) {
+				quantitat = quantitat + (lloguer.getDies() -2) * 2.5;
+			}
+			break;
+
+		case Vehicle.LUXE:
+			quantitat = quantitat + lloguer.getDies() * 6;
+			break;
+		}
+		return quantitat;
 	}
 }
